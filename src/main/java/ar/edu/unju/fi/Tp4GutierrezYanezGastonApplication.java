33419f596e59a5fe18e1b6331c90b8afd3b0b4da
Gaston <Gaston@DESKTOP-Q182OMJ>
2024-05-23 14:41:34 -0300

    Commit de inicializacion

---
+++ src/main/java/ar/edu/unju/fi/Tp4GutierrezYanezGastonApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4GutierrezYanezGastonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4GutierrezYanezGastonApplication.class, args);
+	}
+
+}
